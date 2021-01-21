0cedfd23c4f33f42e759707524ef5d0fedf39259
Dominik <poplawd@gmail.com>
2021-01-21 13:10:58 +0100

    Model Institution, changed max length on Category model

--- portfolio_lab/app_portfolio/models.py
+++ portfolio_lab/app_portfolio/models.py
@@ -1,5 +1,19 @@
 from django.db import models
 
 
+INSTITUTION_TYPES = (
+    (1, "Fundation"),
+    (2, "Non-goverment organization"),
+    (3, "Local collection"),
+)
+
+
 class Category(models.Model):
-    name = models.CharField(max_length=255)
+    name = models.CharField(max_length=100)
+
+
+class Institution(models.Model):
+    name = models.CharField(max_length=100)
+    description = models.CharField(max_length=300)
+    type = models.IntegerField(choices=INSTITUTION_TYPES, default=1)
+    categories = models.ManyToManyField(Category, related_name="institutions")
