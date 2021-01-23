73fc74ee929b38f6a6da22a13252dcb23ccbb062
Dominik <poplawd@gmail.com>
2021-01-23 15:51:36 +0100

    Register form handling, error displaing in template

--- portfolio_lab/app_portfolio/views.py
+++ portfolio_lab/app_portfolio/views.py
@@ -1,6 +1,10 @@
+import django
+from django.contrib.auth.models import User
 from app_portfolio.models import Donation, Institution
-from django.shortcuts import render
+from django.shortcuts import redirect, render
 from django.views import View
+from django.contrib.auth import login
+from django.contrib import messages
 
 
 class LandingPage(View):
@@ -23,4 +27,39 @@ class Login(View):
 
 class Register(View):
     def get(self, request):
+        storage = messages.get_messages(request)
+        for message in storage:
+            del message
         return render(request, "register.html")
+
+    def post(self, request):
+        first_name = request.POST.get("name")
+        last_name = request.POST.get("surname")
+        email = request.POST.get("email")
+        password = request.POST.get("password")
+        password2 = request.POST.get("password2")
+        if Register.register_user(
+            request, first_name, last_name, email, password, password2
+        ):
+            return redirect("/login/")
+        else:
+            return redirect("/register/")
+
+    @staticmethod
+    def register_user(request, first_name, last_name, email, password, password2):
+        if password == password2 and not User.objects.filter(email=email).exists():
+            User.objects.create_user(
+                username=email,
+                first_name=first_name,
+                last_name=last_name,
+                email=email,
+                password=password,
+            )
+
+            return True
+        elif User.objects.filter(email=email).exclude():
+            messages.add_message(request, messages.WARNING, "Email is already taken")
+            return False
+        else:
+            messages.add_message(request, messages.WARNING, "Passwords do not match")
+            return False
