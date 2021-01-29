f8c0b6c64db04ffa1b0c0eb9d00007762208c2b8
Dominik <poplawd@gmail.com>
2021-01-29 19:21:21 +0100

    getting data from form and showing it at end step

--- portfolio_lab/app_portfolio/static/js/app.js
+++ portfolio_lab/app_portfolio/static/js/app.js
@@ -183,7 +183,6 @@ document.addEventListener("DOMContentLoaded", function () {
       this.$stepInstructions = form.querySelectorAll(".form--steps-instructions p");
       const $stepForms = form.querySelectorAll("form > div");
       this.slides = [...this.$stepInstructions, ...$stepForms];
-
       this.init();
     }
 
@@ -214,6 +213,7 @@ document.addEventListener("DOMContentLoaded", function () {
           e.preventDefault();
           this.currentStep--;
           this.updateForm();
+          this.stepOneCheckboxEvent();
         });
       });
 
@@ -225,7 +225,9 @@ document.addEventListener("DOMContentLoaded", function () {
      * Update form front-end
      * Show next or previous section etc.
      */
+
     updateForm() {
+      this.stepOneCheckboxEvent();
       this.$step.innerText = this.currentStep;
       // TODO: Validation
 
@@ -234,39 +236,108 @@ document.addEventListener("DOMContentLoaded", function () {
         if (slide.dataset.step == this.currentStep) {
           slide.classList.add("active");
         }
-        if (this.currentStep === 1) {
-          let checkboxes = slide.querySelectorAll(".active > .form-group--checkbox > label > .checkbox");
-          checkboxes.forEach(elem => {
-            elem.addEventListener("click", event => {
-              if (elem.parentElement.querySelector("input").classList.contains("selected")) {
-                elem.parentElement.querySelector("input").classList.remove("selected");
-              } else {
-                elem.parentElement.querySelector("input").classList.add("selected");
-              }
-            })
-          })
-        };
         if (this.currentStep === 3) {
-          let selected_categories_array = [];
-          this.slides[6].parentElement.children[0].querySelectorAll(".form-group--checkbox > label > .selected").forEach(item => {
-            selected_categories_array.push(item.value)
+          let selectedCategoriesArray = [];
+          this.slides[6].parentElement.children[0].querySelectorAll("div.selected > label > input").forEach(item => {
+            selectedCategoriesArray.push(item.value)
           })
 
-          let institutions = this.slides[6].querySelectorAll(".form-group--checkbox")
+          let institutions = this.slides[6].querySelectorAll(".form-group--checkbox");
+          console.log(selectedCategoriesArray)
           institutions.forEach(institute => {
-            if (!institute.classList.contains(selected_categories_array)) {
+            if (!institute.classList.contains(selectedCategoriesArray)) {
               institute.style.display = "none";
             }
           })
         }
       });
+      this.checkIfRadioChecked();
+
+      if (this.currentStep === 4) {
+        let buttonStepFour = this.$form.querySelector(".form--steps-container > form > [data-step='4']").querySelector(".next-step");
+        buttonStepFour.addEventListener("click", e => {
+          let firstFormColumn = buttonStepFour.parentElement.previousElementSibling.firstElementChild;
+          let streetName = firstFormColumn.children[1].querySelector("input").value;
+          let cityName = firstFormColumn.children[2].querySelector("input").value;
+          let postalCode = firstFormColumn.children[3].querySelector("input").value;
+          let phoneNumber = firstFormColumn.children[4].querySelector("input").value;
+
+          let secondFormColumn = buttonStepFour.parentElement.previousElementSibling.lastElementChild;
+          let pickupDate = secondFormColumn.children[1].querySelector("input").value;
+          let pickupHour = secondFormColumn.children[2].querySelector("input").value;
+          let additionalInfo = secondFormColumn.children[3].querySelector("textarea").value;
+
+          let chosenOrganization = buttonStepFour.parentElement.parentElement.previousElementSibling.querySelector("div.selected > label > span.description > div.title").innerText;
+          let amountOfBags = buttonStepFour.parentElement.parentElement.previousElementSibling.previousElementSibling.querySelector("div.form-group--inline > label > input").value;
+          let itemsToDonate = [];
+          this.$form.querySelector(".form--steps-container > form > [data-step='1']").querySelectorAll("div.selected > label > span.description").forEach(
+            item => {
+              itemsToDonate.push(item.innerText)
+            }
+          );
+
+
+          if (this.currentStep === 5) {
+            let finalizationForm = this.$form.querySelector(".form--steps-container > form > [data-step='5']").querySelector("div.summary");
+            let bagsAndOrganisationInfo = finalizationForm.querySelector("div.form-section").querySelector("ul");
+            if (amountOfBags === "1") {
+              bagsAndOrganisationInfo.firstElementChild.querySelector("span.summary--text").innerText += `1 worek ${itemsToDonate}`
+            } else if (amountOfBags > 1 && amountOfBags < 5) {
+              bagsAndOrganisationInfo.firstElementChild.querySelector("span.summary--text").innerText += `${amountOfBags} worki ${itemsToDonate}`
+            } else {
+              bagsAndOrganisationInfo.firstElementChild.querySelector("span.summary--text").innerText += `${amountOfBags} worków ${itemsToDonate}`
+            }
+            bagsAndOrganisationInfo.lastElementChild.querySelector("span.summary--text").innerText = `Dla organizacji ${chosenOrganization}`
+            let deliveryPickupInfo = finalizationForm.querySelectorAll("div.form-section--column");
+            console.log(deliveryPickupInfo)
+            let deliveryPickupAddress = deliveryPickupInfo[0].querySelector("ul").children;
+            deliveryPickupAddress[0].innerText = streetName;
+            deliveryPickupAddress[1].innerText = cityName;
+            deliveryPickupAddress[2].innerText = postalCode;
+            deliveryPickupAddress[3].innerText = phoneNumber;
+
+            let deliveryPickupDateTime = deliveryPickupInfo[1].querySelector("ul").children;
+            deliveryPickupDateTime[0].innerText = pickupDate;
+            deliveryPickupDateTime[1].innerText = pickupHour;
+            if (additionalInfo) {
+              deliveryPickupDateTime[2].innerText = additionalInfo;
+            } else {
+              deliveryPickupDateTime[2].innerText = "Brak uwag"
+            }
+
+          }
+        })
+
+      }
 
       this.$stepInstructions[0].parentElement.parentElement.hidden = this.currentStep >= 6;
       this.$step.parentElement.hidden = this.currentStep >= 6;
 
-      // TODO: get data from inputs and show them in summary
     }
 
+    checkIfRadioChecked() {
+      let radioCheboxes = this.$form.querySelector(".form--steps-container > form > [data-step='3']").querySelectorAll("div.form-group--checkbox > label > input");
+      radioCheboxes.forEach(checkbox => {
+        if (checkbox.checked) {
+
+          checkbox.parentElement.parentElement.classList.add("selected");
+        }
+
+      });
+    }
+    stepOneCheckboxEvent() {
+      let checkboxes = this.$form.querySelector(".form--steps-container > form > [data-step='1']").querySelectorAll("div.form-group--checkbox > label > input");
+      checkboxes.forEach(elem => {
+        elem.addEventListener("change", event => {
+
+          if (elem.checked) {
+            elem.parentElement.parentElement.classList.add("selected");
+          } else {
+            elem.parentElement.parentElement.classList.remove("selected");
+          }
+        })
+      })
+    };
     /**
      * Submit form
      *
