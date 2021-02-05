01715bdb49d5aea6926bc459e941f0322184a04c
Dominik <poplawd@gmail.com>
2021-02-05 16:52:59 +0100

    Added category model to admin panel

--- portfolio_lab/app_portfolio/admin.py
+++ portfolio_lab/app_portfolio/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
-from app_portfolio.models import Institution
+from app_portfolio.models import Category, Institution
 
 
 admin.site.register(Institution)
+admin.site.register(Category)
