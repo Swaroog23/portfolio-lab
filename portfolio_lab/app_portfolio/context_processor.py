795f5076df01aae708ed8ba9713b437a428d7b17
Dominik <poplawd@gmail.com>
2021-02-06 15:16:46 +0100

    Bugfix in rendering institutions in index, now institutions split in 3 variables for index

--- portfolio_lab/app_portfolio/context_processor.py
+++ portfolio_lab/app_portfolio/context_processor.py
@@ -2,8 +2,16 @@ from app_portfolio.models import Institution, Category
 
 
 def institution_and_category_context(request):
+    categories = Category.objects.all()
+    institutions = Institution.objects.all()
+    foundations = institutions.filter(type=1)
+    non_gov_organisations = institutions.filter(type=2)
+    local_collections = institutions.filter(type=3)
     ctx = {
-        "categories": Category.objects.all(),
-        "institutions": Institution.objects.all(),
+        "categories": categories,
+        "institutions": institutions,
+        "foundations": foundations,
+        "non_gov_organisations": non_gov_organisations,
+        "local_collections": local_collections,
     }
     return ctx
